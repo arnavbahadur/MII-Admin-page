a93346072186a33d4053a33f08c9911ea4b04515
Aman <amansahu5051@gmail.com>
2023-04-09 15:07:12 +0530

    header and  footer

--- my-app/src/Pages/Universal/Footer/Footer.jsx
+++ my-app/src/Pages/Universal/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Footer from './Footer.css'
+import './Footer.css'
 
 const Footer = () => {
   return (
     <div>
       <footer class="footer fixed-bottom">
          <div class="row text-center p-3">
-            <p class="small text-muted">Develop & Design by @Software Development Cell</p>
+            <p class="footer-text"><small> Develop & Design by @Software Development Cell</small></p>
         </div>
         </footer>
     </div>
