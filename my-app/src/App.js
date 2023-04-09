38025d7449e32381d93ef798f21443932aebfab0
Aman <amansahu5051@gmail.com>
2023-04-09 18:56:59 +0530

    blogs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Admin from './Admin'
 import Header from './Pages/Universal/Header/Header'
 import Footer from './Pages/Universal/Footer/Footer'
-
+import Blog from './Pages/Blog/Blog'
 function App() {
   return (
     <div>
       <Header/>
-      <Admin/>
+      {/* <Admin/> */}
+      <Blog/>
       <Footer/>
     </div>
   )
--- my-app/src/Pages/Universal/Footer/Footer.jsx
+++ my-app/src/Pages/Universal/Footer/Footer.jsx
@@ -4,10 +4,8 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div>
-      <footer class="footer fixed-bottom">
-         <div class="row text-center p-3">
-            <p class="footer-text"><small> Develop & Design by @Software Development Cell</small></p>
-        </div>
+      <footer class="footer">
+            <p class="footer-text"> Develop & Design by @Software Development Cell</p>
         </footer>
     </div>
   )
